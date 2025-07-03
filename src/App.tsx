fa5f0320544847d5e2b8148a97332d31ecf54075
Siyun He <hesiyun0603@gmail.com>
2025-07-02 20:03:23 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Labs from './Labs';
+
+function App() {
+  return(
+    <div>
+      <h1>Hello World</h1>
+      <Labs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Labs/index.tsx
@@ -0,0 +1,12 @@
+import Lab1 from './Lab1';
+import Lab2 from './Lab2';
+export default function Labs() {
+    return (
+        <div>
+            <h2>Labs</h2>
+            <p>Welcom to the lab section!</p>
+            <Lab1 />
+            <Lab2 />
+        </div>    
+        )
+}
